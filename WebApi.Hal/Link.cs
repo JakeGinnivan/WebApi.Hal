a8145c2b9e5a8115f259201e1e6f066f19e8d115
Lyndon Gingerich <gingerichlyndon@gmail.com>
2025-09-16 10:37:07 -0500

    Remove dependency on Tavis.UriTemplates
    We copied that code into the project.

--- WebApi.Hal/Link.cs
+++ WebApi.Hal/Link.cs
@@ -1,8 +1,6 @@
 using System;
 using System.Collections.Generic;
-using System.Reflection;
 using System.Text.RegularExpressions;
-using Tavis.UriTemplates;
 using WebApi.Hal.Interfaces;
 
 namespace WebApi.Hal
