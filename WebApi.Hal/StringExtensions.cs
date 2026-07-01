101c6d8dd00bb2196337269bf03f6f33c84a0719
Lyndon Gingerich <gingerichlyndon@gmail.com>
2026-07-01 10:45:04 -0500

    Obsolete StringExtensions and StringExtensions.Replace

--- WebApi.Hal/StringExtensions.cs
+++ WebApi.Hal/StringExtensions.cs
@@ -3,8 +3,10 @@ using System.Text;
 
 namespace WebApi.Hal
 {
+    [Obsolete("Will be removed in a future release", error: false)]
     public static class StringExtensions
     {
+        [Obsolete("Use string.Replace(string, string?, StringComparison) instead", error: false)]
         public static string Replace(this string str, string oldValue, string newValue, StringComparison comparison)
         {
             var sb = new StringBuilder();
